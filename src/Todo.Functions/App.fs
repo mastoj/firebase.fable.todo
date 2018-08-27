205cd94df880dbb2b7e4c1f92e81c0e1933e6cc3
Tomas Jansson <tomas.jansson@unacast.com>
2018-08-27 23:16:04 +0200

    Working hosting and functions

--- src/Todo.Functions/App.fs
+++ src/Todo.Functions/App.fs
@@ -5,24 +5,6 @@ open Fable.Import.Firebase.Functions
 open Fable.Import
 open System
 
-module ExpressHelpers =
-    let toRequestHandler handler =
-        new System.Func<express.Request, express.Response, (obj -> unit), obj>(handler)
-
-    let returnJson (res: express.Response) x =
-        res.setHeader("Content-Type", U2.Case1 "application/json")
-        res.json(x) |> box
-
-    let post url handler (app: express.Express) =
-        let requestHandler = handler |> toRequestHandler
-        app.post (U2.Case1 url, requestHandler) |> ignore
-        app
-
-    let get path handler (app: express.Express) =
-        let requestHandler = handler |> toRequestHandler
-        app.get(U2.Case1 path, requestHandler) |> ignore
-        app
-
 module App =
 
     let app = express.Invoke()
